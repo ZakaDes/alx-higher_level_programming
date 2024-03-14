e72e41d3fc9800061d7b039f13090ee7c39d04d8
ZakaDes <zakarialahmiyed@gmail.com>
2024-03-14 16:03:08 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     n = dir(hidden_4)
-    for i in n:
-        if n[0:2] != "__":
-            print(i)
+    l = len(n)
+    for i in range(0, l):
+        if n[i][:2] !=  "__":
+            print(n[i])
