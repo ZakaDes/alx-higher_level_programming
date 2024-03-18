f64acc881ae4ea04bbc495639e98c1ac68823750
ZakaDes <zakarialahmiyed@gmail.com>
2024-03-18 16:22:04 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx >= len(my_list):
-        return none
-    elif idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return none
     else:
         return my_list[idx]
