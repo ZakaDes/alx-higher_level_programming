1fbe3ae923b359952f25e722fb29f86c19638a99
ZakaDes <zakarialahmiyed@gmail.com>
2024-04-15 15:33:32 -0700

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""create a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """sub-class of list"""
+    def __init__(self):
+        """init the object"""
+        super().__init__()
+    def print_sorted(self):
+        """print the sorted list"""
+        print(sorted(self))
