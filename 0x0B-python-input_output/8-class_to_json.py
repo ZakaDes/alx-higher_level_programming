6cf18e3f94e9e55ca7e7922fc3fc32c669703128
ZakaDes <zakarialahmiyed@gmail.com>
2024-04-16 16:42:56 -0700

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ a function that returns the dictionary description"""
+
+
 def class_to_json(obj):
     """return the dictionary of a simple data structure"""
     return obj.__dict__
